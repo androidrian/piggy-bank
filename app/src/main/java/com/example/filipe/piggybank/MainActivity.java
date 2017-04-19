886793f705878056fd1c8ac9b839bf8db14da6bf
Filipe Conceicao <1040523@isep.ipp.pt>
2017-04-19 22:37:16 +0100

    just recovered from a big error, had to edit the manifest and string cause it didn't find the class after I renamed some files.

--- app/src/main/java/com/example/filipe/piggybank/MainActivity.java
+++ app/src/main/java/com/example/filipe/piggybank/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
